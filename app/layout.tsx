b0523c32ab7bc3a1cf08822873284ac36b2a53ee
BSam-B3 <surapong3331@gmail.com>
2026-03-01 02:12:20 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'จงเจริญ | Jong Jaroen',
+    description: 'Local Freelance Marketplace for Prasae Community by PandVHappiness',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+          <html lang="th">
+                <body className="bg-gray-50 min-h-screen">{children}</body>body>
+          </html>html>
+        )
+}</html>
